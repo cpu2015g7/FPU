7f6f6f72d405143d1a00c8399d79f6ba20d62475
kumatheworld <kumatheworld@yahoo.co.jp>
2015-11-17 09:20:28 +0900

    modify slightly

--- f2i/f2i.vhd
+++ f2i/f2i.vhd
@@ -11,13 +11,13 @@ end f2i;
 architecture f2ichan of f2i is
   signal shift : std_logic_vector (3 downto 0);
   signal tmp4, tmp3, tmp2, tmp1, tmp : std_logic_vector (31 downto 0);
-  signal pos, neg, which : std_logic_vector (30 downto 0);
+  signal which : std_logic_vector (30 downto 0);
 begin
   f2i_out <= x"00000000" when f2i_in(30 downto 23) < x"7e" else
              x"80000000" when f2i_in(30 downto 23) >= x"9e" else
              f2i_in(31) & which;
-  which <= pos when f2i_in(31) = '0' else
-           neg;
+  which <= tmp(31 downto 1) + (x"0000000" & "00" & tmp(0)) when f2i_in(31) = '0' else
+           not tmp(31 downto 1) + (x"0000000" & "00" & (not tmp(0)));
   
   shift <= f2i_in(27 downto 24) + x"1";
   
@@ -30,10 +30,5 @@ begin
   tmp1 <= "00" & tmp2(31 downto 2) when shift(0) = '0' else
           tmp2;
   tmp <= "0" & tmp1(31 downto 1) when f2i_in(23) = '0' else
-          tmp1; 
-
-  pos <= tmp(31 downto 1) + (x"0000000" & "00" & tmp(0));
-  
-  neg <= not pos + ("000" & x"0000001");
-  
+          tmp1;
 end f2ichan;
