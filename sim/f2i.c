3211abd49cbba96cbcf354ed662fd0484890ace7
kumatheworld <kumatheworld@yahoo.co.jp>
2015-11-21 10:04:45 +0900

    revise slightly

--- sim/f2i.c
+++ sim/f2i.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdint.h>
 
-static uint32_t downto(uint32_t a, int x, int y) {
+uint32_t downto(uint32_t a, int x, int y) {
   return x >= 31 ? (a >> y) : (a >> y) & ((1 << (x-y+1)) - 1);
 }
 
@@ -9,7 +9,7 @@ uint32_t f2i(uint32_t a) {
   uint32_t tmp, shift, which;
   shift = (downto(a, 27, 24) + 1) % 0x10;
   tmp = (0x80000000 | (downto(a, 22, 0) << 8)) >> (31 - ((shift << 1) | downto(a, 23, 23)));
-  which = (a >> 31) & 1 ? downto(~tmp, 31, 1) + !(tmp&1) : downto(tmp, 31, 1) + (tmp&1);
+  which = (a >> 31) ? downto(~tmp, 31, 1) + !(tmp&1) : downto(tmp, 31, 1) + (tmp&1);
   if (downto(a, 30, 23) < 0x7e)
     return 0x00000000;
   else if (downto(a, 30, 23) >= 0x9e)
